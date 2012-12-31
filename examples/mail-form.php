ce8f4c6e89c02708f5af7d4170901b5ad089517f
Michał Moroz <michalmoroz@gmail.com>
2012-12-31 17:55:38 +0100

    fixes in mail-form example

--- examples/mail-form.php
+++ examples/mail-form.php
@@ -12,35 +12,37 @@ meta('translation-field-empty', '[left blank]');
 $form = new \Silverplate\Form;
 
 // Then add some text fields to the form
-$f->add('name', TextField::make('Full name', true));
-$f->add('e-mail', TextField::make('E-mail address', true));
+$form->add('name', TextField::make('Full name', true));
+$form->add('e-mail', TextField::make('E-mail address', true));
 
 // The second parameter in Field constructor describes if a field is required
-$f->add('phone', TextField::make('Your phone'));
+$form->add('phone', TextField::make('Your phone'));
 
 // The third paramter in Field constructor specifies initial value for a field
-$f->add('company', TextField::make('Company', false, 'n/a'));
+$form->add('company', TextField::make('Company', false, 'n/a'));
 
 // You can also add other types of fields
-$f->add('message', TextareaField::make('Your message', true));
+$form->add('message', TextareaField::make('Your message', true));
 
-$f->add('topic', ChoiceField::make('Message topic', true)->choices(array(
+$form->add('topic', ChoiceField::make('Message topic', true)->choices(array(
     'General inquiry',
     'Job offer',
     'I would love to speak to someone out there',
 )));
 
-$f->add('know-you', BooleanField::make('Do you know me?'));
+$form->add('know-you', BooleanField::make('Do you know me?'));
 
 // Or add a custom validator to a field
-$f->add('question', TextField::make('2 + 2 =', true)->validate(function($value) {
-    return trim($value) === '4';
-});
+$form->add('question', TextField::make('2 + 2 =', true)->validate(function($value) {
+    if(trim($value) !== '4') {
+        return 'This answer is not valid.';
+    }
+}));
 
 // If form is valid, then we can send an e-mail 
-if($_SERVER['REQUEST_METHOD'] == 'POST' && $f->valid()) {
+if($_SERVER['REQUEST_METHOD'] == 'POST' && $form->valid()) {
     // Set the file path to the mail template file
-    Mailer::send_form($f, __DIR__ . '/mail-template.php');
+    Mailer::send_form($form, __DIR__ . '/mail-template.php');
 
     // Change this URL to match your domain
     throw new Http302('http://mysite.com/thank-you');
@@ -52,13 +54,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && $f->valid()) {
  * If you want to group form fields and put them in different 
  * layout blocks, use the Form::request() method:
  *
- * foreach($f->request(array('name', 'e-mail', 'phone')) as $field):
+ * foreach($form->request(array('name', 'e-mail', 'phone')) as $field):
  */
 
 ?>
 
 <form action="" method="post">
-<?php foreach($f as $field): ?>
+<?php foreach($form as $field): ?>
     <div class="field <?php if($field instanceof BooleanField): ?>checkbox<?php endif; ?>">
         <div class="label">
             <?php echo $field->label() ?>
